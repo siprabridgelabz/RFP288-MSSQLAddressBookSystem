aa7281d65ad85b58b78a5a54bb616dc98b544429
sipra <sipramishra085@gmail.com>
2023-08-05 00:35:08 +0530

    [Sipra]Add:Draw ER Diagram for ServiceBOOK

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -63,3 +63,68 @@ VALUES('Priya','Jain','Konkani','Kunnur','Kerela',755047,9668325835,'priyajain@g
 ('Nakul','Thakur','Gorakhpur','Louknow','UP',755145,8658325835,'nakulthakur@gmail.com','Friend','Profession');
 
 SELECT*FROM AddressBookDB
+
+--UC12--Draw the ER Diagram for Address Book Service DB
+CREATE TABLE AddressTableDB(PersonID INT PRIMARY KEY IDENTITY(1,1),
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(30) NOT NULL,
+Address VARCHAR(100) NOT NULL,
+City VARCHAR(20) NOT NULL,
+State VARCHAR(20) NOT NULL,
+Zip INT NOT NULL,
+PhoneNumber BIGINT NOT NULL,
+EmailID VARCHAR(50) NOT NULL);
+
+--ID CustNo Name Address
+CREATE TABLE Employee(
+ID INT PRIMARY KEY IDENTITY(1,1),
+CustNo INT NOT NULL,
+NAME VARCHAR(20) NOT NULL,
+Address VARCHAR(150) NOT NULL);
+
+SELECT*FROM Employee
+
+CREATE TABLE EmployeeCompany(
+CompanyName VARCHAR(50),
+Location VARCHAR(40),
+CustNo INT PRIMARY KEY IDENTITY(1,1),
+ID INT,
+FOREIGN KEY(ID) REFERENCES Employee(ID))
+
+SELECT*FROM EmployeeCompany
+SELECT*FROM Employee
+
+INSERT INTO Employee VALUES(1,'Sipra','OD'),
+(2,'Mani','TL'),
+(3,'Kruti','MH'),
+(4,'Nikita','AP')
+
+INSERT INTO EmployeeCompany VALUES( 'APY','MP',1),
+( 'KUY','BL',2),
+( 'NEWY','KL',3),
+( 'SYS','KA',4)
+
+SELECT Employee.Name, EmployeeCompany.CompanyName FROM Employee INNER JOIN EmployeeCompany
+ON Employee.ID= EmployeeCompany.ID
+SELECT*FROM EmployeeCompany
+SELECT*FROM Employee
+
+SELECT Employee.Name, EmployeeCompany.CompanyName FROM Employee LEFT JOIN EmployeeCompany
+ON Employee.ID= EmployeeCompany.ID
+SELECT*FROM EmployeeCompany
+SELECT*FROM Employee
+
+SELECT Employee.Name, EmployeeCompany.CompanyName FROM Employee RIGHT JOIN EmployeeCompany
+ON Employee.ID= EmployeeCompany.ID
+SELECT*FROM EmployeeCompany
+SELECT*FROM Employee
+
+SELECT Employee.Name, EmployeeCompany.CompanyName FROM Employee RIGHT JOIN EmployeeCompany
+ON Employee.ID= EmployeeCompany.ID
+SELECT*FROM EmployeeCompany
+SELECT*FROM Employee
+
+SELECT Employee.Name, EmployeeCompany.CompanyName FROM Employee FULL OUTER JOIN EmployeeCompany
+ON Employee.ID= EmployeeCompany.ID
+SELECT*FROM EmployeeCompany
+SELECT*FROM Employee
